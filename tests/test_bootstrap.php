9017aeb70d06e0fd7d8ff5974947a1471640d57b
Shawn McCool <shawn@mccool.email>
2023-11-11 14:04:53 +0100

    clean up testing code a tad bit

--- tests/test_bootstrap.php
+++ tests/test_bootstrap.php
@@ -1,4 +1,6 @@
-<?php namespace Tests;
+<?php
+
+namespace Tests;
 
 use Throwable;
 
@@ -18,7 +20,7 @@ function it(string $description, callable $test): void
 
     if (empty($exceptions)) {
         $filename = basename(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[0]['file']);
-        echo "{$filename} :: it {$description}\n";
+        echo test_description_display($filename, $description);
         return;
     }
 
@@ -26,21 +28,25 @@ function it(string $description, callable $test): void
         echo "\n";
 
         foreach ($exceptions as $exception) {
-
             // naive method to find the most relevant trace frame
             $traceFrames = array_filter(
                 $exception->getTrace(),
-                fn($frame) => $frame['file'] != __FILE__ && basename($frame['file']) !== 'run.php'
+                fn ($frame) => $frame['file'] != __FILE__ && basename($frame['file']) !== 'run.php'
             );
             $frame = reset($traceFrames);
 
             // which file, which test
             $testFile = basename(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[0]['file']);
-            echo "{$testFile} ;; it {$description}\n";
+            echo test_description_display($testFile, $description);
 
             // display the exception
-            $frameFile = basename($frame['file']);
-            echo str_pad('', strlen($testFile) + 4, ' ', STR_PAD_LEFT) . $frameFile . ':' . $frame['line'] . ' - ' . get_class($exception) . ' - ' . ($exception->getMessage() ? '"' . $exception->getMessage() . '"' : '') . "\n";
+            echo exception_display(
+                $testFile,
+                basename($frame['file']),
+                $frame['line'],
+                $exception::class,
+                $exception->getMessage()
+            );
         }
 
         echo "\n";
@@ -57,7 +63,9 @@ function expectException(string $exceptionClass, callable $test): void
             return;
         }
 
-        throw new InvalidAssertion("Expected exception '{$exceptionClass}' but received '" . get_class($exception) . "'.");
+        throw new InvalidAssertion(
+            "Expected exception '{$exceptionClass}' but received '" . get_class($exception) . "'."
+        );
     }
 
     throw new InvalidAssertion("Expected exception '{$exceptionClass}' but received no exception.");
@@ -69,12 +77,13 @@ function expectEqual(mixed $expected, mixed $actual): void
         return;
     }
 
-    throw new InvalidAssertion("Expected " . var_export($expected, true) . " but got " . var_export($actual, true) . ".");
+    throw new InvalidAssertion(
+        "Expected " . var_export($expected, true) . " but got " . var_export($actual, true) . "."
+    );
 }
 
 function expectTrue(bool $expected): void
 {
-
     if ($expected === true) {
         return;
     }
@@ -82,11 +91,29 @@ function expectTrue(bool $expected): void
     throw new InvalidAssertion("Expected value to be 'true' but got 'false'.");
 }
 
-function expectFalse(bool $expected) {
-
+function expectFalse(bool $expected)
+{
     if ($expected === false) {
         return;
     }
 
     throw new InvalidAssertion("Expected value to be 'false' but got 'true'.");
+}
+
+function test_description_display(
+    string $filename,
+    string $description
+): string {
+    return "$filename :: it {$description}\n";
+}
+
+function exception_display(
+    string $testFile,
+    string $exceptionThrowInFile,
+    string $thrownInFileLineNumber,
+    string $exceptionClass,
+    string $exceptionMessage
+): string {
+    return str_pad('', strlen($testFile) + 4, ' ', STR_PAD_LEFT)
+        . "$exceptionThrowInFile : $thrownInFileLineNumber - $exceptionClass - $exceptionMessage\n";
 }
